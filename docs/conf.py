1924dc6f0125ee14046a9c8d03a54c553af5e9c4
dedenbangkit <mail@dedenbangkit.com>
2024-10-11 17:20:36 +0700

    [#8] Add read the docs

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+# -- General configuration ------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = []
+
+project = 'USAID WSSH Tool 1'
+copyright = '2024, Akvo'
+author = 'Deden Bangkit'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# These patterns also affect html_static_path and html_extra_path
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output ----------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
